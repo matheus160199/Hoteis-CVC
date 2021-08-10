6963ca0cf1d0ef2a5e9df5d76dfcb471a943c760
matheus160199 <ribamatriba@gmail.com>
2021-08-10 18:35:58 -0300

    first commit

---
+++ src/main/java/br/com/cvc/hoteis/HoteisApplication.java
@@ -0,0 +1,13 @@
+package br.com.cvc.hoteis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteisApplication.class, args);
+	}
+
+}
